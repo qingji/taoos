d88a05144671cf7f1e17de7356eee65934ad5e39
Tao <hetaoing@gmail.com>
2013-01-27 20:03:40 +0800

    day5_harib02i init GDT and IDT

--- src/bootpack.c
+++ src/bootpack.c
@@ -12,8 +12,11 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 void init_screen(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
 
-
+//void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+//void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define COL8_000000		0
 #define COL8_FF0000		1
 #define COL8_00FF00		2
@@ -39,20 +42,33 @@ struct BOOTINFO {
 	char *vram;
 };
 
+struct SEGMENT_DESCRIPTOR{
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR{
+	short offset_low, selector;
+	char dw_count,access_right;
+	short offset_high;
+};
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-	char s[40];
+	char s[40], mcursor[256];
+	int mx, my;
 	
 	init_palette();
-	
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-	putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
-	putfonts8_asc(binfo->vram, binfo->scrnx, 32, 32, COL8_000000, "Tao OS.");
-	putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Tao OS.");
 	
-	sprintf(s, "scrnx = %d", binfo->scrnx);
+	mx = (binfo->scrnx - 16) / 2; //桌面中央
+	my = (binfo->scrny - 28 - 16) / 2;
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	
+	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 	
 	for(;;)
@@ -64,21 +80,21 @@ void HariMain(void)
 void init_palette(void)
 {
 	static unsigned char table_rgb[16 * 3] = {
-		0x00, 0x00, 0x00,	/*  0:黑 */
+		0x00, 0x00, 0x00,	/*  0:黑*/
 		0xff, 0x00, 0x00,	/*  1:亮红 */
 		0x00, 0xff, 0x00,	/*  2:亮绿 */
-		0xff, 0xff, 0x00,	/*  3:亮黄 */
-		0x00, 0x00, 0xff,	/*  4:亮蓝 */
+		0xff, 0xff, 0x00,	/*  3:亮黄*/
+		0x00, 0x00, 0xff,	/*  4:亮蓝*/
 		0xff, 0x00, 0xff,	/*  5:亮紫 */
 		0x00, 0xff, 0xff,	/*  6:浅亮色 */
 		0xff, 0xff, 0xff,	/*  7:白 */
 		0xc6, 0xc6, 0xc6,	/*  8:亮灰 */
 		0x84, 0x00, 0x00,	/*  9:暗红 */
-		0x00, 0x84, 0x00,	/* 10:暗率 */
-		0x84, 0x84, 0x00,	/* 11:暗黄 */
+		0x00, 0x84, 0x00,	/* 10:暗绿 */
+		0x84, 0x84, 0x00,	/* 11:暗黄*/
 		0x00, 0x00, 0x84,	/* 12:暗青 */
 		0x84, 0x00, 0x84,	/* 13:暗紫 */
-		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
+		0x00, 0x84, 0x84,	/* 14:浅暗蓝*/
 		0x84, 0x84, 0x84	/* 15:暗灰 */
 	};
 	
@@ -90,7 +106,7 @@ void init_palette(void)
 void set_palette(int start, int end, unsigned char *rgb)
 {
 	int i, eflags;
-	eflags = io_load_eflags(); // 加载IF(终端标志)
+	eflags = io_load_eflags(); // 加载IF
 	io_cli(); // IF置为0
 	io_out8(0x03c8, start);
 	for(i = start; i <= end; i++)
@@ -166,3 +182,103 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char color, unsigned cha
 	}
 	return;
 }
+
+void init_mouse_cursor8(char *mouse, char bc)
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
+	}
+	return;
+}
+
+void init_gdtidt(void)
+{
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+	struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR *)0x0026f800;
+	int i;
+	//init GDT
+	for(i = 0; i < 8192; i++) {
+		set_segmdesc(gdt + i, 0, 0, 0);
+	}
+	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);//4GB
+	set_segmdesc(gdt + 1, 0x0007ffff, 0x00280000, 0x4092);//512KB
+	
+	//init IDT
+	for(i = 0; i < 256; i++){
+		set_gatedesc(idt + i, 0, 0, 0);
+	}
+	load_gdtr(0xffff, 0x00270000);
+	
+	return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	if(limit > 0xfffff){
+		ar != 0x8000;//G_bit = 1
+		limit /= 0x1000;
+	}
+	sd->limit_low = limit & 0xffff;
+	sd->base_low =  base & 0xffff;
+	sd->base_mid = (base >> 16) & 0xff;
+	sd->access_right = ar &0xff;
+	sd->limit_high = ((limit >> 16)& 0x0f) |((ar >> 8) &0xf0);
+	sd->base_high = (base >> 24) & 0xff;
+	return;
+	
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+	gd->offset_low = offset & 0xffff;
+	gd->selector = selector;
+	gd->dw_count = (ar >> 8) & 0xff;
+	gd->access_right = ar & 0xff;
+	gd->offset_high = (offset >> 16) & 0xffff;
+	return;
+}
+
+
+
